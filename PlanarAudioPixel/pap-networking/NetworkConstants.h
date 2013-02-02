80ba96c98d33b90f9bfe2b7228dc43731063d6bf
Giancarlo Devich <giancarlodevich@gmail.com>
2013-02-02 15:38:07 -0600

    Added the const specifier to variables in pap-networking/NetworkConstants.h.

--- PlanarAudioPixel/pap-networking/NetworkConstants.h
+++ PlanarAudioPixel/pap-networking/NetworkConstants.h
@@ -11,10 +11,11 @@ namespace Networking
 	// The maximum number of clients who can be connected to the playback server
 	// simultaneously. If the number of clients connected meets this number, then
 	// any further connections should be rejected.
-	unsigned int MAX_SIMULTANEOUS_CLIENTS = 30;
+	const unsigned int MAX_SIMULTANEOUS_CLIENTS = 30;
 
 	// The minimum volume threshold which can be reached before the server simply
 	// rounds the volume calculation down to 0. This is used to prevent clients from
 	// wasting time playing samples at a volume which could not be heard.
-	float MIN_VOLUME_THRESHOLD = 0.025f;
+	const float MIN_VOLUME_THRESHOLD = 0.025f;
+
 }
