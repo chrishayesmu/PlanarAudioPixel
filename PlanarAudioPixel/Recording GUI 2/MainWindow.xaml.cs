98bcea5e49e5e40e212924e86df77bbbd0f351df
Ryan Donnelly <rjdgy5@mail.missouri.edu>
2013-02-07 16:14:18 -0800

    Work on Recording GUI
    Most layout and basic functionality done

--- PlanarAudioPixel/Recording GUI 2/MainWindow.xaml.cs
+++ PlanarAudioPixel/Recording GUI 2/MainWindow.xaml.cs
@@ -12,18 +12,30 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-//using WMPLib;
+using System.IO;
 
 namespace Recording_GUI_2
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
+        private string filename;
+        private MediaPlayer mplayer = new MediaPlayer();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //If the user presses space, call the Record_Click function
+            this.PreviewKeyDown += delegate(object sender, KeyEventArgs e)
+            {
+                if (e.Key.ToString() == "Space") {
+                    this.Record_Click(null, null);   
+                }
+            };
         }
 
         private void FileBrowse_Click(Object sender, EventArgs e)
@@ -35,8 +47,7 @@ namespace Recording_GUI_2
 
             // Set filter for file extension and default file extension
             dlg.DefaultExt = ".mp3";
-            dlg.Filter = "Audio Files (.mp3)|*.mp3";
-
+            dlg.Filter = "Audio Files (.wav)|*.wav";
 
             // Display OpenFileDialog by calling ShowDialog method
             Nullable<bool> result = dlg.ShowDialog();
@@ -45,18 +56,61 @@ namespace Recording_GUI_2
             if (result == true)
             {
                 // Open document
-                string filename = dlg.FileName;
+                filename = dlg.FileName;
                 FileNameTextBox.Text = filename;
+                string file = System.IO.Path.GetFileName(filename);
+                FileNameLabel.Content = file;
             }
         }
-        /*
+        
         //Play button
         private void Play_Click(Object sender, EventArgs e)
         {
-            WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
-            player.URL = @"track.mp3";
-            player.controls.play();
-        }*/
+            //Check to make sure that a file was selected
+            if (filename == null)
+            {
+                return;
+            }
+                //Play the audio back to the user
+                mplayer.Open(new Uri(filename, UriKind.Relative));
+                mplayer.Play();
+
+                //In time with the audio, show the drawn audio path
+
+        }
+
+        private void Record_Click(Object sender, EventArgs e)
+        { 
+            //Check to make sure a file was selected
+            if (filename == null) {
+                return;
+            }
+
+            //Play the audio when the user is drawing a path
+            mplayer.Open(new Uri(filename, UriKind.Relative));
+            mplayer.Play();
+   
+            //Record the user's mouse movement for the audio path
+            
+         
+        }
+
+        private void Reset_Click(Object sender, EventArgs e)
+        {
+           //Pop up with message to confirm reset without saving
+            Reset_Popup.IsOpen = true;
+        }
+
+            //Reset popup window
+            private void Reset_Yes(Object sender, EventArgs e)
+            {
+                
+            }
+
+            private void Reset_Cancel(Object sender, EventArgs e)
+            {
+
+            }
     }
 }
 
