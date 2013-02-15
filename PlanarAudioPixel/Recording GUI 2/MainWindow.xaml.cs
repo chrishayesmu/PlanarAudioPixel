c1b33a32163d61a323ca98eb423ecf6e44c344b1
Ryan Donnelly <rjdgy5@mail.missouri.edu>
2013-02-14 16:07:29 -0800

    Added many things regarding Recording GUI
    Too many to name, yo

--- PlanarAudioPixel/Recording GUI 2/MainWindow.xaml.cs
+++ PlanarAudioPixel/Recording GUI 2/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using System.IO;
 
 namespace Recording_GUI_2
@@ -24,6 +25,7 @@ namespace Recording_GUI_2
     {
         private string filename;
         private MediaPlayer mplayer = new MediaPlayer();
+        private DispatcherTimer _timer;
 
         public MainWindow()
         {
@@ -38,9 +40,175 @@ namespace Recording_GUI_2
             };
         }
 
+        //Slider Controls
+        /*
+         *      http://msdn.microsoft.com/en-us/library/windows/apps/xaml/hh986967.aspx
+         * 
+                void videoMediaElement_MediaEnded(object sender, RoutedEventArgs e)
+                {
+                    StopTimer();
+                    timelineSlider.Value = 0.0;
+                }
+
+                private void SetupTimer()
+                {
+                    _timer = new DispatcherTimer();
+                    _timer.Interval = TimeSpan.FromSeconds(timelineSlider.StepFrequency);
+                    StartTimer();
+                }
+
+                private void _timer_Tick(object sender, object e)
+                {
+                    if (!_sliderpressed)
+                    {
+                        timelineSlider.Value = mplayer.Position.TotalSeconds;
+                    }
+                }
+
+                private void StartTimer()
+                {
+                    _timer.Tick += _timer_Tick;
+                    _timer.Start();
+                }
+
+                private void StopTimer()
+                {
+                    _timer.Stop();
+                    _timer.Tick -= _timer_Tick;
+                }
+
+                private double SliderFrequency(TimeSpan timevalue)
+                {
+                    double stepfrequency = -1;
+
+                    double absvalue = (int)Math.Round(
+                        timevalue.TotalSeconds, MidpointRounding.AwayFromZero);
+
+                    stepfrequency = (int)(Math.Round(absvalue / 100));
+
+                    if (timevalue.TotalMinutes >= 10 && timevalue.TotalMinutes < 30)
+                    {
+                        stepfrequency = 10;
+                    }
+                    else if (timevalue.TotalMinutes >= 30 && timevalue.TotalMinutes < 60)
+                    {
+                        stepfrequency = 30;
+                    }
+                    else if (timevalue.TotalHours >= 1)
+                    {
+                        stepfrequency = 60;
+                    }
+
+                    if (stepfrequency == 0) stepfrequency += 1;
+
+                    if (stepfrequency == 1)
+                    {
+                        stepfrequency = absvalue / 100;
+                    }
+
+                    return stepfrequency;
+                }
+                */
+        //Called when audio is playing
+        //Record mouse movements here
+        void audioMediaElement_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            string mousePosition = Mouse.GetPosition(Application.Current.MainWindow).ToString();
+            MessageBoxResult result = MessageBox.Show(mousePosition);
+            MessageBoxResult results = MessageBox.Show("Audio is playing");
+        }
+
+        //Called when the audio has finished playing
+        //Save the mouse movements to a file here
+        public void audioMediaElement_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            //StopTimer();
+            timelineSlider.Value = 0.0;
+            MessageBoxResult results = MessageBox.Show("Audio finished playing");
+        }
+
+        //Called when there is an error opening the file
+        public void audioMediaElement_MediaFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            MessageBoxResult results = MessageBox.Show("Error: Audio could not be opened");
+        }
+
+        //Play button
+        private void Play_Click(Object sender, EventArgs e)
+        {
+            //Check to make sure that a file was selected
+            if (filename == null)
+            {
+                return;
+            }
+            //Play the audio back to the user
+            //This player JUST plays the audio, nothing else
+            mplayer.Open(new Uri(filename, UriKind.Relative));
+            mplayer.Play();
+
+            /*
+            audioMediaElement.Source = new Uri(filename,  UriKind.Relative);
+            audioMediaElement.Play();
+            */
+
+            //In time with the audio, show the drawn audio path
+
+            //Start the slider
+            /*double absvalue = (int)Math.Round(
+            mplayer.NaturalDuration.TimeSpan.TotalSeconds,
+            MidpointRounding.AwayFromZero);
+
+                timelineSlider.Maximum = absvalue;
+                timelineSlider.StepFrequency = SliderFrequency(videoMediaElement.NaturalDuration.TimeSpan);
+                SetupTimer();*/
+        }
+
+        //Record button
+        private void Record_Click(Object sender, EventArgs e)
+        {
+            //Check to make sure a file was selected
+            if (filename == null)
+            {
+                MessageBoxResult results = MessageBox.Show("Error: You must select a file first");
+                return;
+            }
+
+            //Play the audio when the user is drawing a path
+            audioMediaElement.Source = new Uri(filename, UriKind.Relative);
+            audioMediaElement.Play();
+
+            //Record the user's mouse movement for the audio path
+            //This is done in the audioMediaElement_MediaOpened function
+
+            //Start the slider
+            /*double absvalue = (int)Math.Round(
+            mplayer.NaturalDuration.TimeSpan.TotalSeconds, MidpointRounding.AwayFromZero);
+
+                        timelineSlider.Maximum = absvalue;
+                        timelineSlider.StepFrequency = SliderFrequency(videoMediaElement.NaturalDuration.TimeSpan);
+                        SetupTimer();*/
+        }
+
+        //Reset button
+        private void Reset_Click(Object sender, EventArgs e)
+        {
+            audioMediaElement.Stop();
+            MessageBoxResult result = MessageBox.Show(this, "If you close this window, all data will be lost.",
+             "Confirmation", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.OK)
+            {
+                // Yes code here
+            }
+            else
+            {
+                // No code here
+            }
+
+        }
+
+        //File Browse Button
         private void FileBrowse_Click(Object sender, EventArgs e)
         {
-            //FileNameTextBox.Text = "Hello";
 
             // Create OpenFileDialog
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
@@ -62,55 +230,6 @@ namespace Recording_GUI_2
                 FileNameLabel.Content = file;
             }
         }
-        
-        //Play button
-        private void Play_Click(Object sender, EventArgs e)
-        {
-            //Check to make sure that a file was selected
-            if (filename == null)
-            {
-                return;
-            }
-                //Play the audio back to the user
-                mplayer.Open(new Uri(filename, UriKind.Relative));
-                mplayer.Play();
-
-                //In time with the audio, show the drawn audio path
-
-        }
-
-        private void Record_Click(Object sender, EventArgs e)
-        { 
-            //Check to make sure a file was selected
-            if (filename == null) {
-                return;
-            }
-
-            //Play the audio when the user is drawing a path
-            mplayer.Open(new Uri(filename, UriKind.Relative));
-            mplayer.Play();
-   
-            //Record the user's mouse movement for the audio path
-            
-         
-        }
-
-        private void Reset_Click(Object sender, EventArgs e)
-        {
-           //Pop up with message to confirm reset without saving
-            Reset_Popup.IsOpen = true;
-        }
-
-            //Reset popup window
-            private void Reset_Yes(Object sender, EventArgs e)
-            {
-                
-            }
-
-            private void Reset_Cancel(Object sender, EventArgs e)
-            {
-
-            }
     }
 }
 
